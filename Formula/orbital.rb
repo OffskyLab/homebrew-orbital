8131de5cbaa56b9b7de7d46956fbf019e4dc27bf
Grady Zhuo <gradyzhuo@gmail.com>
2026-04-09 01:24:34 +0800

    [UPDATE] fill sha256 for v0.1.7 pre-built binaries

--- Formula/orbital.rb
+++ Formula/orbital.rb
@@ -7,18 +7,18 @@ class Orbital < Formula
   on_macos do
     on_arm do
       url "https://github.com/OffskyLab/Orbital/releases/download/v#{version}/orbital-darwin-arm64.tar.gz"
-      sha256 "PLACEHOLDER_DARWIN_ARM64"
+      sha256 "db457f218f8c8a0ff38aca38d3dd70332ccc61db55ec9f4f2d622a1fc4bf5a03"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/OffskyLab/Orbital/releases/download/v#{version}/orbital-linux-x86_64.tar.gz"
-      sha256 "PLACEHOLDER_LINUX_X86_64"
+      sha256 "f2646ed761f0cf853e71946bd3c6ef11dbb753a875369f1b192178a1b6f6fabb"
     end
     on_arm do
       url "https://github.com/OffskyLab/Orbital/releases/download/v#{version}/orbital-linux-arm64.tar.gz"
-      sha256 "PLACEHOLDER_LINUX_ARM64"
+      sha256 "5c25ead7837a08a1b1cc149ac3098d88c0b6b2c9e8c540a4f7123217d9170a7e"
     end
   end
 
