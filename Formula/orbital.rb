2bdc52daa463fadf59a03cd9ba6a89dcbe0a0288
Grady Zhuo <gradyzhuo@gmail.com>
2026-04-11 21:15:16 +0800

    [UPDATE] orbital 1.1.0 (rebuild with brew update fix)

--- Formula/orbital.rb
+++ Formula/orbital.rb
@@ -7,18 +7,18 @@ class Orbital < Formula
   on_macos do
     on_arm do
       url "https://github.com/OffskyLab/Orbital/releases/download/v#{version}/orbital-darwin-arm64.tar.gz"
-      sha256 "2f2e1b8e3ef7e3a7985e18d9aff96012a3cfd97bd3bde4df56b0197b2528dd1e"
+      sha256 "5faba67e890b03193de3235a83113792dfda2dfa92200f090464be2ce6bc9965"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/OffskyLab/Orbital/releases/download/v#{version}/orbital-linux-x86_64.tar.gz"
-      sha256 "f4c3427207d278944abd0e6cec8cbb4ed3d77a8f42557f4db6f66fd42c6b7fa1"
+      sha256 "60e41cabf632985c8b9c888dbf5a60d4a062da0627da764475aae0c397f4f9e8"
     end
     on_arm do
       url "https://github.com/OffskyLab/Orbital/releases/download/v#{version}/orbital-linux-arm64.tar.gz"
-      sha256 "9b3ba8fe6b56e886acafde4b351e8ba44e04553dc956918d52fea0d654d7d96e"
+      sha256 "fd4ca6ceaa48dd4e923ad4d56fe0de5b49c0d6f1c9f358d4e59016e7b0b3386c"
     end
   end
 
